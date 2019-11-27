440f8fdcbb09f1f60869d41d0d6da079259faebc
mthcsta <matheuscostakrenn@gmail.com>
2019-11-26 23:15:25 -0300

    Corrigido bug dos tiros no salva estado

--- inc/estado.c
+++ inc/estado.c
@@ -12,7 +12,7 @@ void salvarEstado(int nivel, int posicao, boneco_t jogador, int pontuacao, tiro_
     fwrite(&posicao, sizeof(int), 1, arquivo);
     fwrite(&jogador, sizeof(boneco_t), 1, arquivo);
     fwrite(&pontuacao, sizeof(int), 1, arquivo);
-    fwrite(tiro, sizeof(MAX_TIROS), 1, arquivo);
+    fwrite(tiro, sizeof(tiro_t), MAX_TIROS, arquivo);
     fwrite(&inimigos_existentes, sizeof(int), 1, arquivo);
     fwrite(inimigo, sizeof(boneco_t), inimigos_existentes, arquivo);
     fwrite(&animacao, sizeof(int), 1, arquivo);
@@ -41,7 +41,7 @@ void carregarEstado(FILE * salve, int *posicao, boneco_t *jogador, int *pontuaca
     fread(posicao, sizeof(int), 1, salve);
     fread(jogador, sizeof(boneco_t), 1, salve);
     fread(pontuacao, sizeof(int), 1, salve);
-    fread(tiro, sizeof(MAX_TIROS), 1, salve);
+    fread(tiro, sizeof(tiro_t), MAX_TIROS, salve);
     fread(inimigos_existentes, sizeof(int), 1, salve);
     fread(inimigo, sizeof(boneco_t), *inimigos_existentes, salve);
     fread(animacao, sizeof(int), 1, salve);
